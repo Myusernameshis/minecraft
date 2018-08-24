7fac97cc3ab1a322ec638320daced674d4ed96b2
Myusernameshis <42595926+Myusernameshis@users.noreply.github.com>
2018-08-24 14:57:06 +0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 22 09:19:08 2018
+Created on Fri Aug 24 09:21:54 2018
 
-@author: Sam
+@author: NTPU
 """
-
+from time import sleep
 from mcpi.minecraft import Minecraft
 sam = Minecraft.create()
-
-x,y,z=sam.player.getTilePos()
-sam.setSign(x,y,z,63,0,"W","","","L")
+sleep(2)
+myID=sam.getPlayerEntityID("sam0617")
+sam.postToTitle(myID,"§天亮了","§you are a human")
--- untitled1.py
+++ untitled1.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 21 14:08:10 2018
+Created on Fri Aug 24 09:28:21 2018
 
-@author: Sam
+@author: NTPU
 """
+
+from random import choice
 from mcpi.minecraft import Minecraft
 sam = Minecraft.create()
-x,y,z=sam.player.getTilePos()
-width=10
-length=6
-height=20
-block=11
-air=0
-sam.setBlocks(x,y,z,x+width,y+height,z+length,block)
-sam.setBlocks(x+1,y+1,z+1,x+width-1,y+height-1,z+length-1,air)
+
+list2 = [[1,1,0,1,1,1,1,1,1,0],
+         [1,1,1,1,1,0,1,1,1,0],
+         [1,1,1,1,1,1,1,0,1,0],
+         [1,1,1,0,1,1,0,1,1,0],
+         [0,1,1,1,1,1,1,1,1,0],
+         [1,1,0,1,1,0,1,1,1,0],
+         [1,1,1,1,0,1,1,1,1,0],
+         [0,1,0,1,1,1,0,1,1,0],
+         [1,1,1,0,1,1,1,1,0,0]]
+
+myID=sam.getPlayerEntityId("sam0617")
+x,y,z=sam.entity.getTilePos(myID)
+StartX = x
+
+for list1 in list2:
+    for i in list1:
+        sam.setBlock(x,y,z,i)
+        x=x+1
+    x = StartX
+    z=z+1
